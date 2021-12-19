afe7d5536f9e1bf9a48344a41ab8400eb9a2af86
mateu52 <53394614+mateu52@users.noreply.github.com>
2021-12-19 05:19:56 +0100

    valid hook update

---
+++ src/component/List.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/jsx-no-undef */
+import "./userList.css";
+//import { Link } from 'react-router-dom';
+import React from "react";
+import UserDetail from "./UserDetail";
+const List = ({ data }) => {
+  return (
+    <div className="userCss">
+      {data.map((user) => (
+        <div key={user.login.uuid} className="userMap">
+          <Link to={`/UserDetail/${user.login.uuid}`}>
+            <UserDetail
+              name={user.name.first}
+              surname={user.name.last}
+              picture={user.picture.large}
+              location={user.location.street.name}
+              location1={user.location.street.number}
+              city={user.location.city}
+              email={user.email}
+              date={user.registered.date}
+            />
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
